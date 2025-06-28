3d6a3289dfb2c6b743e4e100ad2b838ba2dbb634
Camillo <49236712+cpmlreef@users.noreply.github.com>
2025-06-28 16:59:54 +0200

    Update php.php

--- php/php.php
+++ php/php.php
@@ -22,6 +22,12 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
+{
+  "require": {
+    "monolog/monolog": "^2.0"
+  }
+}
+
 class MultiDomainPanel extends PanelProvider
 {
     public function panel(Panel $panel): Panel
